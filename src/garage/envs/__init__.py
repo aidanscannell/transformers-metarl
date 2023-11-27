f2d7b6cb36a24b77fd6d5ddb9e5ae210613358fb
Aidan Scannell <scannell.aidan@gmail.com>
2023-11-27 15:50:10 +0200

    remove metaworl

--- src/garage/envs/__init__.py
+++ src/garage/envs/__init__.py
@@ -2,7 +2,8 @@
 
 from garage.envs.grid_world_env import GridWorldEnv
 from garage.envs.gym_env import GymEnv
-from garage.envs.metaworld_set_task_env import MetaWorldSetTaskEnv
+
+# from garage.envs.metaworld_set_task_env import MetaWorldSetTaskEnv
 from garage.envs.multi_env_wrapper import MultiEnvWrapper
 from garage.envs.normalized_env import normalize
 from garage.envs.point_env import PointEnv
@@ -10,12 +11,12 @@ from garage.envs.task_name_wrapper import TaskNameWrapper
 from garage.envs.task_onehot_wrapper import TaskOnehotWrapper
 
 __all__ = [
-    'GymEnv',
-    'GridWorldEnv',
-    'MetaWorldSetTaskEnv',
-    'MultiEnvWrapper',
-    'normalize',
-    'PointEnv',
-    'TaskOnehotWrapper',
-    'TaskNameWrapper',
+    "GymEnv",
+    "GridWorldEnv",
+    # 'MetaWorldSetTaskEnv',
+    "MultiEnvWrapper",
+    "normalize",
+    "PointEnv",
+    "TaskOnehotWrapper",
+    "TaskNameWrapper",
 ]
